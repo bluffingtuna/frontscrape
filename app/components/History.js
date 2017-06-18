19ae3f2218adff160a4d87247adcee411487b105
John A.T. Nguyen <johnatnguyen@gmail.com>
2017-06-18 12:26:37 -0700

    front-end getting there

--- app/components/History.js
+++ app/components/History.js
@@ -24,12 +24,16 @@ class History extends Component {
     History
   </div>
   <div className="card-block">
-<div class="card">
-  <div class="card-block">
-    <h4 className="card-title">Google<a href="www.google.com" className="listings btn btn-danger">Erase History</a></h4>
-    <p className="card-text">www.google.com</p>
+    <h4 className="card-title"><a href="https://www.google.com">Google</a><a href="#" className="listings btn btn-warning" title="Click to Erase">Erase History</a></h4>
+    <p className="card-text">https://www.google.com</p>
   </div>
-</div>
+  <div className="card-block">
+    <h4 className="card-title"><a href="https://www.bing.com">Bing</a><a href="#" className="listings btn btn-warning" title="Click to Erase">Erase History</a></h4>
+    <p className="card-text">https://www.bing.com</p>
+  </div>
+  <div className="card-block">
+    <h4 className="card-title"><a href="https://www.facebook.com">Facebook</a><a href="#" className="listings btn btn-warning" title="Click to Erase">Erase History</a></h4>
+    <p className="card-text">https://www.facebook.com</p>
   </div>
 </div>
 		);
