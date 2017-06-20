a2daa697302927a7686b135ba5cc2487382d25b2
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-06-20 13:00:21 -0700

    ssl certificate added

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ var app = express();
 const http = require('http').Server(app);//instantiates node-native http server based on app
 const io = require('socket.io')(http);//mounts io protocol on native server
 var db = process.env.MONGODB_URI || "mongodb://localhost/frontscrape"; //sets up database from environment variable, defaults to mongodb localhost
+//SSL Certificate for HTTPS
+var fs = require('fs'),
+    express = require('express'),
+    https = require('https');
+
+var https_options = {
+  key: fs.readFileSync(__dirname +'/ssl-key.pem'),
+  cert: fs.readFileSync(__dirname +'/ssl-cert.pem')
+};
+var HOST = "localhost";
 
 // Connect mongoose to our database
 mongoose.connect(db, err=>{
@@ -21,12 +31,21 @@ mongoose.connect(db, err=>{
   }
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public")); //serves public folder
 require("./routes/routes")(app, io); //imported function sets up routes and io logic;
 
 // Start the server
-http.listen(PORT, function() {
-  console.log(`Now listening on port ${PORT}`);
-});
+var server = https.createServer(https_options, app)
+                  .listen(PORT, HOST);
+
+console.log('+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+');
+console.log('HTTPS Server listening @ https://%s:%s', HOST, PORT);
+console.log('+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+');
